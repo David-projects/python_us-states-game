10bf923567b98e21fea3c4b2b2dd8b2375f3b9d7
DESKTOP-M98ST0G\david <davidtheo@gmail.com>
2023-11-05 11:49:22 +0000

    Updated the code to use comprehension lists and put the states that needs to be learnt into a file.

--- main.py
+++ main.py
@@ -11,27 +11,31 @@ screen.addshape(IMAGE)
 turtle.shape(IMAGE)
 
 # load states file
-states = pandas.read_csv(STATES_FILE)
-
+data = pandas.read_csv(STATES_FILE)
+states = data.state.to_list()
+guessed_states = []
+print(states)
 # get answer to question
 answer_state = screen.textinput(title="0/50 Guess the State", prompt="What's a state's name?").capitalize()
 
 game_is_on = True
 score = 0
+loops = 0
 
 while game_is_on:
-    state = states[states['state'] == answer_state]
-
-    if not len(state) == 0:
+    loops += 1
+    if answer_state in states:
+        guessed_states.append(answer_state)
         score += 1
         sam = turtle.Turtle()
         sam.color("black")
         sam.penup()
         sam.hideturtle()
-        sam.goto(int(state['x'].iloc[0]), int(state['y'].iloc[0]))
+        state_data = data[data.state == answer_state]
+        sam.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
         sam.write(arg=answer_state, move=False, align="Center", font=('Arial', 8, 'normal'))
 
-    if score >= len(states):
+    if loops >= len(states):
         game_is_on = False
 
     try:
@@ -41,6 +45,9 @@ while game_is_on:
         game_is_on = False
 
     if answer_state == "Exit":
+        missing_states = [state for state in states if state not in  guessed_states]
+        to_learn = pandas.DataFrame(missing_states)
+        to_learn.to_csv("missing_states.txt")
         game_is_on = False
 
 
